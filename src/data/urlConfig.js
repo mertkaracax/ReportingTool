4f00fd641dc374329c88d6d23d2986416b69732c
Mert KARACA <izm.mertkaraca@hotmail.com>
2023-02-25 15:51:50 +0300

    url change

--- src/data/urlConfig.js
+++ src/data/urlConfig.js
@@ -1,12 +1,8 @@
-
-
-class urlConfig{
-    constructor(){
-
-    }
-    getDefaultUrl(){
-        return "http://190.92.148.116:8080/api/reporting"
-    }
+class urlConfig {
+  constructor() {}
+  getDefaultUrl() {
+    return "https://190.92.148.116:8080/api/reporting";
+  }
 }
 
 export default new urlConfig();
